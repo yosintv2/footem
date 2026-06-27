cdb12e617d86739b0442c2216b360a97d6959a54
Anynomous <156112254+yosintv2@users.noreply.github.com>
2026-06-28 03:09:25 +0900

    fix: set build.format to 'file' for flat .html output

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,9 @@ import sitemap from '@astrojs/sitemap';
 export default defineConfig({
   site: 'https://www.yosintv.net',
   output: 'static',
+  build: {
+    format: 'file',
+  },
   integrations: [
     react(),
     sitemap(),
