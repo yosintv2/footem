3d69a28f35b6e352c89565873cd74cf723e7557e
Anynomous <156112254+yosintv2@users.noreply.github.com>
2026-07-08 03:20:28 +0900

    Use www.yosintv.in as canonical site URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import react from '@astrojs/react';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://yosintv.in',
+  site: 'https://www.yosintv.in',
   output: 'static',
   build: {
     format: 'directory',
